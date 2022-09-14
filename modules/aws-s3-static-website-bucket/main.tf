7345c79ff98bb001b207d4352bfc6d27d963416c
jcorral <joc.palomares@gmail.com>
2022-09-14 12:18:48 -0700

    add bucket configuration module

---
+++ modules/aws-s3-static-website-bucket/main.tf
@@ -0,0 +1,46 @@
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket = var.bucket_name
+
+    tags = var.tags
+  
+}
+
+resource "aws_s3_bucket_website_configuration" "s3_bucket" {
+    bucket = aws_s3_bucket.s3_bucket.id
+
+    index_document {
+      suffix = "index.html"
+    }
+
+    error_document {
+      suffix = "error.html"
+    }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket" {
+    bucket = aws_s3_bucket.s3_bucket.id
+
+    acl = "public-read"
+  
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket" {
+    bucket = aws_s3_bucket.s3_bucket.id
+
+    policy = jsonencode({
+        version = "2012-10-17"
+        statement = [
+            {
+                Sid = "PublicReadGetObject"
+                Effect = "Allow"
+                Principal = "*"
+                Action = "s3:GetObject"
+                Resource = [
+                     aws_s3_bucket.s3_bucket.arn,
+                     "${aws_s3_bucket.s3_bucket.arn}/*",
+                ]
+            },
+        ]
+    })
+  
+}
